988a884cbca556680a81b511889344a44a1eea89
Jong-Shian Wu <js@crypto.tw>
2016-09-21 15:48:20 +0800

    Implement crypto.keygen with openssl(1)

--- crypto.js
+++ crypto.js
@@ -19,17 +19,50 @@ crypto.js
 
 */
 
+const child_process = require('child_process');
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const util = require('util');
+
+
 var hash = function (message) {
   // TODO
   var hashed_message = new Buffer();
   return hashed_message;
 };
 
-var keygen = function () {
-  // TODO
-  var private_key = new Buffer();
-  var public_key = new Buffer();
-  return [private_key, public_key];
+var keygen = exports.keygen = function () {
+  const TMPDIR = os.tmpdir();
+
+  child_process.spawnSync('openssl', [
+    'ecparam',
+    '-genkey',
+    '-name', 'prime256v1',
+    '-outform', 'DER',
+    '-out', path.join(TMPDIR, 'sk.orig'),
+  ]);
+
+  child_process.spawnSync('dd', [
+    'skip=10',
+    'bs=1',
+    'if=' + path.join(TMPDIR, 'sk.orig'),
+    'of=' + path.join(TMPDIR, 'sk'),
+  ]);
+
+  child_process.spawnSync('openssl', [
+    'ec',
+    '-pubout',
+    '-inform', 'DER',
+    '-outform', 'DER',
+    '-in', path.join(TMPDIR, 'sk'),
+    '-out', path.join(TMPDIR, 'pk'),
+  ]);
+
+  const private_key_buffer = fs.readFileSync(path.join(TMPDIR, 'sk'));
+  const public_key_buffer = fs.readFileSync(path.join(TMPDIR, 'pk'));
+
+  return [private_key_buffer, public_key_buffer];
 };
 
 var sign = function (private_key, message) {
