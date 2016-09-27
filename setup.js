a9644006484d15a657dc76bfddfa7dae646d92d1
Jong-Shian Wu <js@crypto.tw>
2016-09-27 19:44:06 +0800

    setup.js should just use sstore.js to store private key

--- setup.js
+++ setup.js
@@ -1,14 +1,23 @@
 const fs = require('fs');
+const sstore = require('./sstore');
 const crypt = require('./crypto');
 const constant = require('./constants');
 
 const main = () => {
-	const keys = crypt.keygen();
-	const pri_key_enc = crypt.encrypt(constant.DEVICE_SSTORE_SECRET_KEY, keys[0]);
-	fs.writeFileSync(constant.PATH_TO_PRIVATE_KEY_FILE, pri_key_enc);
-	fs.writeFileSync(constant.PATH_TO_PUBLIC_KEY_FILE, keys[1]);
+	console.log('Generating a key pair...');
 
-	console.log('Key Files Generated');
+	// generate a key pair
+	const keys = crypt.keygen();
+	const pri_key = keys[0];
+	const pub_key = keys[1];
+
+	// prover stores the private key
+	sstore.write(pri_key);
+
+	// challenger stores the public key
+	fs.writeFileSync(constant.PATH_TO_PUBLIC_KEY_FILE, pub_key);
+
+	console.log('Done');
 };
 
 main();
