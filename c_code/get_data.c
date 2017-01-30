b85cd2a52e1bfe2d7f9989f244705a9d79aa2374
GoogleOnStackOverflow <b01901111@ntu.edu.tw>
2017-01-30 18:42:25 +0800

    get_data.c first implement

--- c_code/get_data.c
+++ c_code/get_data.c
@@ -33,24 +33,44 @@
 
 #include <libvmi/libvmi.h>
 
+void print_hex_string(void* buf, unsigned long size){
+    unsigned long i = 0;
+    for(i = 0; i<size; i++){
+        printf("%02X", ((char*)buf)[i]);
+    }
+}
+
 int main (int argc, char **argv)
 {
+    // Fill this by insert findoffsets module on Dom1
+    unsigned long start_code_offset = 0xe8;
+    unsigned long end_code_offset = 0xf0;
+    unsigned long start_data_offset = 0xf8;
+    unsigned long end_data_offset = 0x100;
+    unsigned long start_brk_offset = 0x108;
+    unsigned long brk_offset = 0x110;
+    unsigned long start_stack_offset = 0x118;
+    unsigned long stack_offset = 0x8;
+    
     vmi_instance_t vmi;
     addr_t list_head = 0, next_list_entry = 0;
     addr_t current_process = 0;
     char *procname = NULL;
-    char *try_to_get_data = NULL;
     vmi_pid_t pid = 0;
     unsigned long tasks_offset = 0, pid_offset = 0, name_offset = 0;
+    unsigned long mm_offset = 0, mm_addr = 0;
+    unsigned long start_stack = 0, stack_pointer = 0, start_brk = 0, brk = 0;
+    unsigned long start_code = 0, end_code = 0, start_data = 0, end_data = 0;
     status_t status;
 
     /* this is the VM or file that we are looking at */
-    if (argc != 2) {
-        printf("Usage: %s <vmname>\n", argv[0]);
+    if (argc != 3) {
+        printf("Usage: %s <vmname> <process name>\n", argv[0]);
         return 1;
-    } // if
+    }
 
     char *name = argv[1];
+    char *target_process = argv[2];
 
     /* initialize the libvmi library */
     if (vmi_init(&vmi, VMI_AUTO | VMI_INIT_COMPLETE, name) == VMI_FAILURE) {
@@ -59,27 +79,22 @@ int main (int argc, char **argv)
     }
 
     /* init the offset values */
+    // Get these data from the config file of the VM (/etc/libvmi.conf)
+    // See: https://libvmi.wordpress.com/2015/01/23/libvmi-xen-setup/
+    // Fill the config file with the output of the findoffset module
     if (VMI_OS_LINUX == vmi_get_ostype(vmi)) {
         tasks_offset = vmi_get_offset(vmi, "linux_tasks");
         name_offset = vmi_get_offset(vmi, "linux_name");
         pid_offset = vmi_get_offset(vmi, "linux_pid");
+        mm_offset = vmi_get_offset(vmi, "linux_mm");
     }
     else if (VMI_OS_WINDOWS == vmi_get_ostype(vmi)) {
-        tasks_offset = vmi_get_offset(vmi, "win_tasks");
-        name_offset = vmi_get_offset(vmi, "win_pname");
-        pid_offset = vmi_get_offset(vmi, "win_pid");
+        printf("Not supporting Windows\n");
+        goto error_exit;
     }
 
-    if (0 == tasks_offset) {
-        printf("Failed to find win_tasks\n");
-        goto error_exit;
-    }
-    if (0 == pid_offset) {
-        printf("Failed to find win_pid\n");
-        goto error_exit;
-    }
-    if (0 == name_offset) {
-        printf("Failed to find win_pname\n");
+    if (0 == tasks_offset || 0 == name_offset || 0 == pid_offset || 0 == mm_offset) {
+        printf("Failed to find offsets\n");
         goto error_exit;
     }
 
@@ -87,7 +102,7 @@ int main (int argc, char **argv)
     if (vmi_pause_vm(vmi) != VMI_SUCCESS) {
         printf("Failed to pause VM\n");
         goto error_exit;
-    } // if
+    }
 
     /* demonstrate name and id accessors */
     char *name2 = vmi_get_name(vmi);
@@ -95,79 +110,87 @@ int main (int argc, char **argv)
     if (VMI_FILE != vmi_get_access_mode(vmi)) {
         uint64_t id = vmi_get_vmid(vmi);
 
-        printf("Process listing for VM %s (id=%"PRIu64")\n", name2, id);
+        printf("Getting data from VM %s (id=%"PRIu64")\n", name2, id);
     }
     else {
-        printf("Process listing for file %s\n", name2);
+        printf("Not supporting file mode\n");
+        goto error_exit;
     }
     free(name2);
 
     /* get the head of the list */
-    if (VMI_OS_LINUX == vmi_get_ostype(vmi)) {
-        /* Begin at PID 0, the 'swapper' task. It's not typically shown by OS
-         *  utilities, but it is indeed part of the task list and useful to
-         *  display as such.
-         */
-        list_head = vmi_translate_ksym2v(vmi, "init_task") + tasks_offset;
-    }
-    else if (VMI_OS_WINDOWS == vmi_get_ostype(vmi)) {
-
-        // find PEPROCESS PsInitialSystemProcess
-        if(VMI_FAILURE == vmi_read_addr_ksym(vmi, "PsActiveProcessHead", &list_head)) {
-            printf("Failed to find PsActiveProcessHead\n");
-            goto error_exit;
-        }
-    }
-
+    list_head = vmi_translate_ksym2v(vmi, "init_task") + tasks_offset;
     next_list_entry = list_head;
 
+    int status_code = 400;
     /* walk the task list */
     do {
-
         current_process = next_list_entry - tasks_offset;
 
-        /* Note: the task_struct that we are looking at has a lot of
-         * information.  However, the process name and id are burried
-         * nice and deep.  Instead of doing something sane like mapping
-         * this data to a task_struct, I'm just jumping to the location
-         * with the info that I want.  This helps to make the example
-         * code cleaner, if not more fragile.  In a real app, you'd
-         * want to do this a little more robust :-)  See
-         * include/linux/sched.h for mode details */
-
-        /* NOTE: _EPROCESS.UniqueProcessId is a really VOID*, but is never > 32 bits,
-         * so this is safe enough for x64 Windows for example purposes */
-        vmi_read_32_va(vmi, current_process + pid_offset, 0, (uint32_t*)&pid);
-
         procname = vmi_read_str_va(vmi, current_process + name_offset, 0);
-        try_to_get_data = vmi_read_va(vmi, 0x08048000, pid, 1024);
 
         if (!procname) {
             printf("Failed to find procname\n");
             goto error_exit;
-        }
+        }else if (0 == strcmp(procname,target_process)){
+            status_code = 200;
+            /* Note: the task_struct that we are looking at has a lot of
+             * information.  However, the process name and id are burried
+             * nice and deep.  Instead of doing something sane like mapping
+             * this data to a task_struct, I'm just jumping to the location
+             * with the info that I want.  This helps to make the example
+             * code cleaner, if not more fragile.  In a real app, you'd
+             * want to do this a little more robust :-)  See
+             * include/linux/sched.h for mode details */
 
-        /* print out the process name */
-        printf("[%5d] %s (struct addr:%"PRIx64")\n", pid, procname, current_process);
+            /* NOTE: _EPROCESS.UniqueProcessId is a really VOID*, but is never > 32 bits,
+             * so this is safe enough for x64 Windows for example purposes */
+            vmi_read_32_va(vmi, current_process + pid_offset, 0, (uint32_t*)&pid);
 
-        if(strcmp(procname,"trivial") == 0){
-            vmi_print_hex_va(vmi, 0x08048000, pid, 1024);
-        }
-        
-        if (procname) {
-            free(procname);
-            free(try_to_get_data);
-            procname = NULL;
+            // Get mm struct addr
+            vmi_read_addr_va(vmi, current_process + mm_offset, 0, &mm_addr);
+
+            // Get addrs of the segments
+            vmi_read_addr_va(vmi, mm_addr + start_code_offset, 0, &start_code);
+            vmi_read_addr_va(vmi, mm_addr + end_code_offset, 0, &end_code);
+            
+            vmi_read_addr_va(vmi, mm_addr + start_data_offset, 0, &start_data);
+            vmi_read_addr_va(vmi, mm_addr + end_data_offset, 0, &end_data);
+
+            vmi_read_addr_va(vmi, mm_addr + start_brk_offset, 0, &start_brk);
+            vmi_read_addr_va(vmi, mm_addr + brk_offset, 0, &brk);
+
+            vmi_read_addr_va(vmi, mm_addr + start_stack_offset, 0, &start_stack);
+            vmi_read_addr_va(vmi, current_process + stack_offset, 0, &stack_pointer);
+            
+            /* print out the process name */
+            printf("[%5d] %s (struct addr:%"PRIx64")\n", pid, procname, current_process);
+            
+            char* code_buffer[end_code-start_code];
+            char* data_buffer[end_data-start_data];
+            char* brk_buffer[brk-start_brk];
+            char* stack_buffer[start_stack-stack_pointer];
+
+            printf("\n{\"status_code\":%d,\"result\":{\"name\":\"%s\",\"pid\":%d,\"code\":",status_code,procname,pid);
+            print_hex_string(code_buffer,vmi_read_va(vmi, start_code, pid, code_buffer, end_code - start_code));
+            printf(",\"data\":\"");
+            print_hex_string(data_buffer,vmi_read_va(vmi, start_data, pid, data_buffer, end_data - start_data));
+            printf("\",\"heap\":\"");
+            print_hex_string(brk_buffer,vmi_read_va(vmi, start_brk, pid, brk_buffer, brk - start_brk));
+            printf("\",\"stack\":\"");
+            print_hex_string(stack_buffer,vmi_read_va(vmi, stack_pointer, pid, stack_buffer, start_stack - stack_pointer));
+            printf("\"}\n");
         }
 
         /* follow the next pointer */
+        free(procname);
+        procname = NULL;
 
         status = vmi_read_addr_va(vmi, next_list_entry, 0, &next_list_entry);
         if (status == VMI_FAILURE) {
             printf("Failed to read next pointer in loop at %"PRIx64"\n", next_list_entry);
             goto error_exit;
         }
-
     } while(next_list_entry != list_head);
 
 
