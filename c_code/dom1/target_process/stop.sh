b62fc7c23918333c2f1272424845534212a46126
GoogleOnStackOverflow <b01901111@ntu.edu.tw>
2017-02-01 17:23:34 +0800

    testing flow

--- c_code/dom1/target_process/stop.sh
+++ c_code/dom1/target_process/stop.sh
@@ -1,1 +1,7 @@
-pidof ./bin/* | xargs kill -9
+pidof trivial | xargs kill -9
+pidof trivial_1 | xargs kill -9
+pidof trivial_2 | xargs kill -9
+pidof trivial_3 | xargs kill -9
+pidof trivial_4 | xargs kill -9
+pidof trivial_5 | xargs kill -9
+pidof trivial_6 | xargs kill -9
--- vmi_tester.js
+++ vmi_tester.js
@@ -1,12 +1,14 @@
 const vmi = require('./vmi');
+const crypt = require('./crypto');
 
 const test1 = () => {
 	var codes_result = [];
 	codes_result.push(vmi.get_state('trivial','code'))
+	console.log("Retrieving Data from Dom-1");
 	for(var i=1; i<7; i++)
 		codes_result.push(vmi.get_state(('trivial_' + i.toString()),'code'))
-	
-	console.log(codes_result[1]['result']);
+	for(var i=0; i<8; i++)
+		console.log(crypt.hash(codes_result[i]['result']['data']));
 }
 
 test1();
