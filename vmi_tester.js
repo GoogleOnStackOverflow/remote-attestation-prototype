31f6e0230b6fc0a40bbd1e490ac1d22dbb9246e2
GoogleOnStackOverflow <b01901111@ntu.edu.tw>
2017-02-01 18:02:27 +0800

    testing assertion added

--- vmi_tester.js
+++ vmi_tester.js
@@ -1,14 +1,50 @@
 const vmi = require('./vmi');
 const crypt = require('./crypto');
+const assert = require('assert');
 
 const test1 = () => {
-	var codes_result = [];
-	codes_result.push(vmi.get_state('trivial','code'))
-	console.log("Retrieving Data from Dom-1");
-	for(var i=1; i<7; i++)
-		codes_result.push(vmi.get_state(('trivial_' + i.toString()),'code'))
-	for(var i=0; i<codes_result.length; i++)
-		console.log(crypt.hash(codes_result[i]['result']['data']).toString('hex'));
+  var codes_result = [];
+  var codes_result_hash = [];
+  codes_result.push(vmi.get_state('trivial','code'))
+  console.log("Retrieving Data from Dom-1");
+  for(var i=1; i<7; i++)
+    codes_result.push(vmi.get_state(('trivial_' + i.toString()),'code'))
+  for(var i=0; i<codes_result.length; i++){
+    assert(codes_result[i]['status_code']===200, 'Error when retrieving data');
+    console.log(codes_result[i]['result']['name'] + ':')
+    codes_result_hash.push(crypt.hash(codes_result[i]['result']['data']));
+    console.log('\t' + codes_result_hash[i].toString('hex'));
+  }
+
+  assert(codes_result_hash[0].equals(
+    codes_result_hash[6]),
+    'Should recognize legal process'
+  );
+
+  assert(!codes_result_hash[0].equals(
+    codes_result_hash[1]),
+    'Should detect static variable init diff'
+  );
+
+  assert(!codes_result_hash[0].equals(
+    codes_result_hash[2]),
+    'Should detect global function return value diff'
+  );
+
+  assert(!codes_result_hash[0].equals(
+    codes_result_hash[3]),
+    'Should detect local variable init value diff'
+  );
+
+  assert(!codes_result_hash[0].equals(
+    codes_result_hash[4]),
+    'Should detect function call diff'
+  );
+
+  assert(!codes_result_hash[0].equals(
+    codes_result_hash[5]),
+    'Should detect variable name diff'
+  );
 }
 
 test1();
