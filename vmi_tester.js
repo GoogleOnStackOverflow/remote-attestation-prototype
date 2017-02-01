420c68591b5ffff5f242897bba3108caf495242c
GoogleOnStackOverflow <b01901111@ntu.edu.tw>
2017-02-01 17:29:36 +0800

    workaround

--- vmi_tester.js
+++ vmi_tester.js
@@ -8,7 +8,6 @@ const test1 = () => {
 	for(var i=1; i<7; i++)
 		codes_result.push(vmi.get_state(('trivial_' + i.toString()),'code'))
 	for(var i=0; i<codes_result.length; i++)
-		console.log(crypt.hash(codes_result[i]['result']['data']));
-}
+		console.log(crypt.hash(codes_result[i]['result']['data']).toString('hex'));
 
 test1();
