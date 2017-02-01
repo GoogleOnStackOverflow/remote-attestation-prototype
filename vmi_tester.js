8c19b18ec289f8393b48b09d7e9a545367b73f22
GoogleOnStackOverflow <b01901111@ntu.edu.tw>
2017-02-01 17:14:06 +0800

    bug fix

--- vmi_tester.js
+++ vmi_tester.js
@@ -3,8 +3,9 @@ const vmi = require('./vmi');
 const test1 = () => {
 	var codes_result = [];
 	codes_result.push(vmi.get_state('trivial','code'))
-	for(var i=1; i<7; i++);
-	codes_result.push(vmi.get_state(('trivial_' + i.toString()),'code'))
+	for(var i=1; i<7; i++)
+		codes_result.push(vmi.get_state(('trivial_' + i.toString()),'code'))
+	
 	console.log(codes_result[1]['result']);
 }
 
