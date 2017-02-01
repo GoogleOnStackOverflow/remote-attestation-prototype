5a9b48279b542950de0f81cb1f28fbdb0c060355
GoogleOnStackOverflow <b01901111@ntu.edu.tw>
2017-02-01 17:28:11 +0800

    bug fix

--- vmi_tester.js
+++ vmi_tester.js
@@ -7,7 +7,7 @@ const test1 = () => {
 	console.log("Retrieving Data from Dom-1");
 	for(var i=1; i<7; i++)
 		codes_result.push(vmi.get_state(('trivial_' + i.toString()),'code'))
-	for(var i=0; i<8; i++)
+	for(var i=0; i<codes_result.length; i++)
 		console.log(crypt.hash(codes_result[i]['result']['data']));
 }
 
